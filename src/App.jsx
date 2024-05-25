ee29e9851d7f5c9603a5f1462def88a22593f094
Priyansh <priyanshpatel234@gmail.com>
2024-05-25 10:13:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Todo from './components/Todo'
+import AddingTodo from './components/AddingTodo'
+
+function App() {
+  
+  return (
+    <>
+      <AddingTodo />
+      <Todo/>
+    </>
+  )
+}
+
+export default App
